03d9c9a4937855099424f78d2609dfe1ae6c36c6
JahinZaman <jahinmz023@gmail.com>
2023-11-14 18:49:45 -0500

    costsupdated4

--- walletwizecost.py
+++ walletwizecost.py
@@ -20,19 +20,23 @@ def calculate_operational_expenditures(num_users, database_paid, jira_paid, dwol
 
 
 def calculate_cost(num_users, cost_options):
-    total_cost = sum(cost for key, (condition, cost) in cost_options.items() if eval(condition) and cost_options[key][2])
+    total_cost = sum(cost for key, option in cost_options.items() if eval(option['condition']) and option['enabled'])
     return total_cost / num_users if num_users > 0 else 0
 st.title('Operational Expenditures Calculator')
+# Slider for number of users
 num_users = st.slider("Number of Users", 1, 20000, 1)
+# Existing checkboxes
 database_paid = st.checkbox("Database Paid ($190/month)")
 jira_paid = st.checkbox("Jira Paid ($110/month)")
 dwolla_upgrade = st.checkbox("Upgrade Dwolla to Paid ($200/month)")
+# Session state to store cost options
 if 'cost_options' not in st.session_state:
     st.session_state.cost_options = {
-        "Database Paid": ("database_paid", 190 if database_paid else 0, database_paid),
-        "Jira Paid": ("jira_paid", 110 if jira_paid else 0, jira_paid),
-        "Dwolla Upgrade": ("dwolla_upgrade", 200 if dwolla_upgrade else 0, dwolla_upgrade)
+        "Database Paid": {"condition": "database_paid", "cost": 190, "enabled": database_paid},
+        "Jira Paid": {"condition": "jira_paid", "cost": 110, "enabled": jira_paid},
+        "Dwolla Upgrade": {"condition": "dwolla_upgrade", "cost": 200, "enabled": dwolla_upgrade}
     }
+# Function to add new cost option
 def add_cost_option():
     option_type = st.session_state.option_type
     if option_type == 'Free until X users':
@@ -46,12 +50,14 @@ def add_cost_option():
         cost = st.number_input("Enter Cost", min_value=0, key='paid_cost')
         condition = "True"
     if st.button("Add Option", key='add_option'):
-        st.session_state.cost_options[f"{option_type} - {st.session_state.new_option}"] = (condition, cost, False)
+        st.session_state.cost_options[f"{option_type} - {st.session_state.new_option}"] = {"condition": condition, "cost": cost, "enabled": False}
+# Add new cost option
 with st.expander("Add New Cost Option"):
     st.session_state.new_option = st.text_input("Option Name")
     st.session_state.option_type = st.selectbox("Select Option Type", ["Free until X users", "Always Free", "Paid"])
     add_cost_option()
-for name, (condition, cost, enabled) in st.session_state.cost_options.items():
-    st.session_state.cost_options[name][2] = st.checkbox(name, value=enabled)
+# Display cost options and calculate cost
+for name, option in st.session_state.cost_options.items():
+    option['enabled'] = st.checkbox(name, value=option['enabled'])
 cost_per_user = calculate_cost(num_users, st.session_state.cost_options)
 st.write(f"Cost per user: ${cost_per_user:.2f}")
