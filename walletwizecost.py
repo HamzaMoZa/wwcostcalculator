2243e91f7b2db56dd699bbda1ab646f01d49e87e
JahinZaman <jahinmz023@gmail.com>
2023-11-14 18:40:04 -0500

    costsupdated2

--- walletwizecost.py
+++ walletwizecost.py
@@ -39,9 +39,22 @@ st.title('Operational Expenditures Calculator')
 #Slider for number of users
 num_users = st.slider("Number of Users", 1, 20000, 1)
 
+#Existing checkboxes
+database_paid = st.checkbox("Database Paid ($190/month)")
+jira_paid = st.checkbox("Jira Paid ($110/month)")
+dwolla_upgrade = st.checkbox("Upgrade Dwolla to Paid ($200/month)") 
 #Session state to store cost options
 if 'cost_options' not in st.session_state:
-    st.session_state.cost_options = {}
+    st.session_state.cost_options = {
+        "Database Paid": ("database_paid", 190 if database_paid else 0),
+        "Jira Paid": ("jira_paid", 110 if jira_paid else 0),
+        "Dwolla Upgrade": ("dwolla_upgrade", 200 if dwolla_upgrade else 0)
+    }
+
+#Update existing costs based on checkbox status
+st.session_state.cost_options["Database Paid"] = ("database_paid", 190 if database_paid else 0)
+st.session_state.cost_options["Jira Paid"] = ("jira_paid", 110 if jira_paid else 0)
+st.session_state.cost_options["Dwolla Upgrade"] = ("dwolla_upgrade", 200 if dwolla_upgrade else 0)
 
 #Function to add new cost option
 def add_cost_option():
@@ -57,13 +70,14 @@ def add_cost_option():
         cost = st.number_input("Enter Cost", min_value=0, key='paid_cost')
         condition = "True"
 
-    st.session_state.cost_options[f"{option_type} - {st.session_state.new_option}"] = (condition, cost)
+    if st.button("Add Option", key='add_option'):
+        st.session_state.cost_options[f"{option_type} - {st.session_state.new_option}"] = (condition, cost)
 
 #Add new cost option
 with st.expander("Add New Cost Option"):
     st.session_state.new_option = st.text_input("Option Name")
     st.session_state.option_type = st.selectbox("Select Option Type", ["Free until X users", "Always Free", "Paid"])
-    st.button("Add Option", on_click=add_cost_option)
+    add_cost_option()
 
 #Display cost options and calculate cost
 for name, (condition, cost) in st.session_state.cost_options.items():
